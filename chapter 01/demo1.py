59995ff9aa92d7431fc1ff709bd8ed882622224b
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-07-01 00:58:39 +0530

    update

---
+++ chapter 01/demo1.py
@@ -0,0 +1,67 @@
+# import pyjokes  # type: ignore
+
+# joke = pyjokes.get_joke()
+# print(joke)
+
+# Question Number 1
+
+# print ("""Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the trav'ller in the dark,
+# Thanks you for your tiny spark,
+# He could not see which way to go,
+# If you did not twinkle so.
+
+# In the dark blue sky you keep,
+# And often thro' my curtains peep,
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# 'Tis your bright and tiny spark,
+# Lights the trav'ller in the dark:
+# Tho' I know not what you are,
+# Twinkle, twinkle, little star.""")
+
+
+# Question Number 3
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Hello Rabiya Naaz")
+# engine.runAndWait()
+
+
+# Question Number 4 or 5 using CHATGPT
+
+# import os
+# def list_directory_contents(directory_path):
+#     try:
+#         # List all files and directories in the specified path
+#         with os.scandir(directory_path) as entries:
+#             for entry in entries:
+#                 print(entry.name)
+#     except FileNotFoundError:
+#         print(f"The directory {directory_path} does not exist.")
+#     except PermissionError:
+#         print(f"Permission denied to access the directory {directory_path}.")
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
+# # Specify the directory path
+# directory_path = "/Users/mzaid/Desktop/PYTHON"
+
+# # Call the function to list the directory contents
+# list_directory_contents(directory_path)
+
+
+ 
+
+
---
+++ chapter 02/04_operators.py
@@ -0,0 +1,15 @@
+# Arithmetic Operators
+
+a = 34
+b = 4
+c = a + b
+print(c)
+print(a)
+
+# Assignment Operators
+a = 4 - 2 
+print(a)
+b = 6
+b += 3
+print(b)
+
