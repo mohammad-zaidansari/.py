93e16a1c3575a3476b24ab729d31a5a52a34037c
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-07-01 16:03:13 +0530

    chapter2

---
+++ chapter 02/02_precticeSet/05_question.py
@@ -0,0 +1,6 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+avg = (a + b)/2
+
+print("Average of two number is : ",avg)
---
+++ chapter 02/02_precticeSet/06_question.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number you want to square: "))
+
+# square = a * a 
+square = a**2
+cube = a**3
+print("Square of number is: ",square)
+print("Cube of number is: ", cube)
