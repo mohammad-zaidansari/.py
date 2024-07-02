feaeccbdd223621e8b5c275c9a5badb2d54b8719
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-07-02 20:47:26 +0530

    chapter5

--- chapter 05/prectice_set_05/07_question.py
+++ chapter 05/prectice_set_05/07_question.py
@@ -1,4 +1,4 @@
 s = {8, 7, 12, "Harry", [1,2]}
 
-first we can not include a list in a set 
-second we naver update the set 
+# first we can not include a list in a set 
+# second we naver update the set 
