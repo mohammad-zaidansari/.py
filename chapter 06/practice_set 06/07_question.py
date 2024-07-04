30d0c91296457b0b18da06cd2de2a61950496235
Mohammad Zaid Ansari <mzaidansari712@gmail.com>
2024-07-04 12:24:00 +0530

    chapter6

---
+++ chapter 06/practice_set 06/07_question.py
@@ -0,0 +1,6 @@
+post = input("Create a post you want to post it: ")
+
+if("Zaid".lower() in post.lower()):
+    print("Yes, this post is containing you name")
+else:
+    print("No, this post is not containing your name")
